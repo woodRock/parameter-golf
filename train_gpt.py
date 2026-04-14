f056a8a01c640d0e9f9024cae182307d5ec69557
woodRock <j.r.h.wood98@gmail.com>
2026-04-14 17:21:34 +1200

    Set full_graph=False

--- train_gpt.py
+++ train_gpt.py
@@ -840,7 +840,7 @@ def main() -> None:
         if isinstance(module, CastedLinear):
             module.float()
     restore_low_dim_params_to_fp32(base_model)
-    compiled_model = torch.compile(base_model, dynamic=False, fullgraph=True)
+    compiled_model = torch.compile(base_model, dynamic=False, fullgraph=False)
     model: nn.Module = DDP(compiled_model, device_ids=[local_rank], broadcast_buffers=False) if distributed else compiled_model
 
     # Optimizer split:
